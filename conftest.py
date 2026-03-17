8edf45506c3e8f4ae284fada072937251faff2d9
maxveresh <maaks388@gmail.com>
2026-03-17 13:16:09 +0300

    feat: add UI API fixtures (api_client_ui, base_url_ui, samokat_url) refactor: update auth headers logic refactor: add type annotations to fixtures and headers, remove unused fixtures

--- conftest.py
+++ conftest.py
@@ -2,14 +2,21 @@ from api.client import ApiClient
 from services.auth_service import AuthService
 from services.users_service import UsersService
 from unittest.mock import Mock
-import pytest
-
+import pytest, os
 
 @pytest.fixture
-def api_client(auth_headers, base_url):
+def api_client(auth_headers: dict, base_url: str):
     client = ApiClient(
         base_url=base_url,
         headers=auth_headers
+    )
+    yield client
+    client.session.close()
+
+@pytest.fixture
+def api_client_ui(base_url_ui: str):
+    client = ApiClient(
+        base_url=base_url_ui,
     )
     yield client
     client.session.close()
@@ -37,16 +44,6 @@ def auth_service_real(api_client):
     return AuthService(client=api_client)
 
 
-@pytest.fixture
-def sample_list():
-    return [1, 2, 3]
-
-
-@pytest.fixture
-def user():
-    return {"name": "Max", "age": 25}
-
-
 @pytest.fixture(scope="session")
 def session_data():
     print("\n[SETUP] Creating session data")
@@ -56,10 +53,21 @@ def session_data():
 @pytest.fixture
 def auth_headers():
     return {
-        'Authorization': 'Bearer my_test_token'
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'User-Agent': 'reqres-qa-tests/1.0',
+        'x-api-key': os.getenv("REQRES_API_KEY")
     }
 
 
 @pytest.fixture
 def base_url():
-    return 'https://httpbin.org/'
+    return 'https://reqres.in/api/'
+
+@pytest.fixture
+def base_url_ui():
+    return 'https://the-internet.herokuapp.com/'
+
+@pytest.fixture
+def samokat_url():
+    return 'https://qa-scooter.praktikum-services.ru/'
