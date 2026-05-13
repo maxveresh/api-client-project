1546914fe0bbf338ea77cdf093e05be07e873d7f
maxveresh <maaks388@gmail.com>
2026-05-13 12:31:45 +0300

    refactor: add Config class with base URLs for multiple apps

---
+++ config/settings.py
@@ -0,0 +1,11 @@
+class Config:
+    BASE_URLS = {
+        'the_internet': 'https://the-internet.herokuapp.com/',
+        'automation_exercise': 'https://automationexercise.com/',
+        'samokat': 'https://qa-scooter.praktikum-services.ru/',
+        'reqres': 'https://reqres.in/api/'
+
+    }
+
+    ENV = "stage"
+
