4000b798092786fdf230a932f51e47140dbfa207
maxveresh <maaks388@gmail.com>
2026-05-20 19:42:25 +0300

    fix: stabilize selenium execution in headless mode for gitlab ci

--- ui/selenium/conftest.py
+++ ui/selenium/conftest.py
@@ -16,6 +16,7 @@ def driver():
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     selenium_url = os.getenv("SELENIUM_URL")
 
